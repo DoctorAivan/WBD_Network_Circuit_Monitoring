2bc9a2ad603a9e37b371044b23a66eef5bad3089
DoctorAivan <design.villarroel.s@gmail.com>
2025-11-17 17:44:28 -0300

    Json Reader

--- core/settings.py
+++ core/settings.py
@@ -201,6 +201,7 @@ else:
     REST_FRAMEWORK = {
         'DEFAULT_RENDERER_CLASSES': (
             'rest_framework.renderers.JSONRenderer',
+            'rest_framework.renderers.BrowsableAPIRenderer', 
         ),
         'DEFAULT_AUTHENTICATION_CLASSES': (
             'rest_framework.authentication.SessionAuthentication',
