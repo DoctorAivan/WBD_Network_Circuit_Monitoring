c8f9cce8d3a56c4ac2a753ad7338355e93bd5404
DoctorAivan <design.villarroel.s@gmail.com>
2026-05-08 17:45:51 -0400

    New command added : remove 30 days

---
+++ api/management/commands/clean_logs_30_days.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from datetime import timedelta
+from api.models import ServerLog
+
+class Command(BaseCommand):
+    help = 'Elimina todos los registros con una antigüedad mayor a 30 días'
+
+    def handle(self, *args, **options):
+        cutoff_date = timezone.now() - timedelta(days=30)
+        deleted_count, _ = ServerLog.objects.filter(timestamp__lt=cutoff_date).delete()
+        
+        self.stdout.write(
+            self.style.SUCCESS(f'Limpieza completada. Se eliminaron {deleted_count} registros más antiguos a 30 días.')
+        )
